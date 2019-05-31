3c381fcbb8aac8a6567e295a0927f6e5f771c33a
yhsol <yhsol1592@gmail.com>
2019-05-31 15:50:04 +0900

    add login secret

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+import { adjectives, nouns } from "./words";
+
+export const secretGenerator = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
