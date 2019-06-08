02ee1ac02d36fcfdca1e25e72ca6afaa5822c340
yhsol <yhsol1592@gmail.com>
2019-06-08 14:49:38 +0900

    add edit post

--- src/fragment.js
+++ src/fragment.js
@@ -1,3 +1,6 @@
+// FULL_POST_FRAGMENT 에서 쓰려는 값들 (USER_FRAGMENT, FILE_FRAGMENT, COMMENT_FRAGMENT) 등을 fragment 형태가 아니라
+// string 형태로 만들어서 써야 작동한다.
+
 export const USER_FRAGMENT = `
         id
         username
