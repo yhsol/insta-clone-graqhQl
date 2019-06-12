c9122068e465ffa102438b3004ec39d58b86d20c
yhsol <yhsol1592@gmail.com>
2019-06-12 20:10:02 +0900

    add new message with computed file

--- src/api/Message/newMessage/newMessage.js
+++ src/api/Message/newMessage/newMessage.js
@@ -1,11 +1,24 @@
-import { subscribe } from "graphql";
+import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Subscription: {
     newMessage: {
       subscribe: (_, args) => {
         const { roomId } = args;
-      }
+        return prisma.$subscribe
+          .message({
+            AND: [
+              { mutation_in: "CREATED" },
+              {
+                node: {
+                  room: { id: roomId }
+                }
+              }
+            ]
+          })
+          .node();
+      },
+      resolve: payload => payload
     }
   }
 };
