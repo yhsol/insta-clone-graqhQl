83616dfdec8cad1afb642a22ca4ba8ce96a71bc5
yhsol <yhsol1592@google.com>
2019-06-25 21:18:21 +0900

    add request secret

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,10 +4,9 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     requestSecret: async (_, args, { request }) => {
-      console.log(request.user);
       const { email } = args;
       const loginSecret = secretGenerator();
-      console.log(loginSecret);
+      console.log(email, loginSecret);
       try {
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
